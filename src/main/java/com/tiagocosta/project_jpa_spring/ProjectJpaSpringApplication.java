18501c67fe3f796f2af3d15875eef905ee1377bb
Tiago Costa <tiagocostasantos0013@gmail.com>
2025-08-25 21:07:31 -0300

    project-springboot3-jpa

---
+++ src/main/java/com/tiagocosta/project_jpa_spring/ProjectJpaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tiagocosta.project_jpa_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJpaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJpaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagocosta/project_jpa_spring/ProjectJpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagocosta.project_jpa_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJpaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
