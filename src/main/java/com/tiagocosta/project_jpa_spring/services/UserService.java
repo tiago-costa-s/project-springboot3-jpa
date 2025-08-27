32199215112a58253496817d3377eaefded5e307
Tiago Costa <tiagocostasantos0013@gmail.com>
2025-08-27 20:32:52 -0300

    Service layer, component registration

---
+++ src/main/java/com/tiagocosta/project_jpa_spring/services/UserService.java
@@ -0,0 +1,27 @@
+package com.tiagocosta.project_jpa_spring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tiagocosta.project_jpa_spring.entities.User;
+import com.tiagocosta.project_jpa_spring.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
