64f73c4af26bd7c8ce2efe5cf38933d85e00442e
Tiago Costa <tiagocostasantos0013@gmail.com>
2025-09-01 19:58:46 -0300

    OrderStatus Enum

--- src/main/java/com/tiagocosta/project_jpa_spring/config/TestConfig.java
+++ src/main/java/com/tiagocosta/project_jpa_spring/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.tiagocosta.project_jpa_spring.entities.Order;
 import com.tiagocosta.project_jpa_spring.entities.User;
+import com.tiagocosta.project_jpa_spring.entities.enums.OrderStatus;
 import com.tiagocosta.project_jpa_spring.repositories.OrderRepository;
 import com.tiagocosta.project_jpa_spring.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "998888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "997777777", "123456");
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:03:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-20T03:43:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-20T15:13:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:03:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-20T03:43:10Z"), OrderStatus.WATTING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-20T15:13:22Z"), OrderStatus.WATTING_PAYMENT, u1);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
